7f47ceb03273ab449a4c11f8476e44af8fa09675
Barbara Fusinska <barbara.fusinska@gmail.com>
2015-11-17 14:03:20 +0100

    Conversions script

---
+++ 2. Conversions.R
@@ -0,0 +1,20 @@
+#implicit
+a <- c(1,2,3)
+a
+a[2] <- "4"
+a
+
+b <-c('a', 'b', 'c')
+b
+b[2] <- 3
+b
+
+#explicit
+is.character(2)
+as.character(2)
+
+is.logical('TRUE')
+as.logical('TRUE')
+
+is.numeric('23')
+as.numeric('23')
