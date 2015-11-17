1f4d9491842157b672a40874476acefecca27613
Barbara Fusinska <barbara.fusinska@gmail.com>
2015-11-17 17:40:57 +0100

    Appending script

---
+++ 3.2 Appending.R
@@ -0,0 +1,24 @@
+#vectors
+a <- c(33, a)
+a
+b <- c(b, 22)
+b
+
+#data frames
+df <- data.frame(a, b)
+df
+
+# binding columns
+c <- 20:30
+df$c <- c
+df
+
+d <- 30:40
+e <- 40:50
+cbind(df, d, e)
+
+#binding rows
+x1 <- c(1,2,3)
+x2 <- c(4,5,6)
+x3 <- c(7,8,9)
+rbind(df,x1, x2, x3)
