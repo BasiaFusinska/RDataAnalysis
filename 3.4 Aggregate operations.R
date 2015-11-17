f9f5c77587d4d3220b834df49c9da5cb6dc9b1c9
Barbara Fusinska <barbara.fusinska@gmail.com>
2015-11-17 18:01:51 +0100

    Aggregate operations script

---
+++ 3.4 Aggregate operations.R
@@ -0,0 +1,13 @@
+#execute function call
+argList <- list(3, 5)
+do.call("*", argList)
+
+#apply accross elements
+myList <- c(1, 4, 9, 16, 25)
+lapply(myList, sqrt)
+
+myList <- c(v1=1, v2=2, v3=3, v4=4)
+lapply(myList, function(x) { x^2})
+
+#returning vector
+sapply(myList, function(x) { x^2})
