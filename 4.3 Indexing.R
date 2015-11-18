b0c52a5af62ec4c67d2859ce6b92728948fbf1c3
Barbara Fusinska <barbara.fusinska@gmail.com>
2015-11-18 17:37:12 +0200

    Indexing script

---
+++ 4.3 Indexing.R
@@ -0,0 +1,16 @@
+#selecting elements
+x <- c(1, 4, 7)
+
+a <- 1:10
+a
+a[x]
+
+b <- as.list(1:10)
+b
+b[x]
+
+#indexing with logicals
+y <- c(TRUE, TRUE, FALSE, TRUE, TRUE, FALSE, FALSE, TRUE, FALSE, FALSE)
+
+a[y]
+b[y]
