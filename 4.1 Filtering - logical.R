24aa89bb4bdef6c27d0a570bc9a69a9581940f90
Barbara Fusinska <barbara.fusinska@gmail.com>
2015-11-18 16:23:03 +0200

    Filterign - logical scripts & task

---
+++ 4.1 Filtering - logical.R
@@ -0,0 +1,20 @@
+#vectors
+a <- 1:10
+a[a > 4]
+
+b <- c("ab", "bc", "abc", "ac", "def")
+b[nchar(b) == 2]
+
+#lists
+A <- as.list(1:10)
+B <- list("ab", "bc", "abc", "ac", "def")
+C <- list(a="ab", b="bc", c="abc", d="ac", e="def")
+
+A[A > 4]
+B[nchar(B) == 2]
+C[nchar(C) == 2]
+
+#Filter
+Filter(function(x) {nchar(x) == 2}, b)
+
+
